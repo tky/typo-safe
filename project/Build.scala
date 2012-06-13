be388e5e2768688a9655407d63d4106cf55e6c34
takayuki murata <takayuki.for.public@gmail.com>
2012-06-14 00:01:13 +0900

    added test settings

--- project/Build.scala
+++ project/Build.scala
@@ -2,6 +2,15 @@ import sbt._
 import sbt.Keys._
 
 object TypoSafeBuild extends Build {
+ val io = Seq(
+    "com.github.scala-incubator.io" %% "scala-io-core" % "0.4.0",
+    "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.0"
+  )
+  val test = Seq(
+    "org.specs2" % "specs2_2.9.1" % "1.9"
+  )
+  val default = io ++ test
+
   lazy val typoSafe = Project(
     id = "typo-safe",
     base = file("."),
@@ -9,7 +18,13 @@ object TypoSafeBuild extends Build {
       name := "typo-safe",
       organization := "com.tk.tp",
       version := "1.0",
-      scalaVersion := "2.9.2"
+      scalaVersion := "2.9.1",
+      resolvers += "scala-tools releases"  at "http://scala-tools.org/repo-releases",
+      resolvers += "scala-tools snapshots" at "http://scala-tools.org/repo-snapshots",
+      resolvers += "releases"  at "http://oss.sonatype.org/content/repositories/releases",
+      resolvers += "snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
+      resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+      libraryDependencies := default
     )
   )
 }
--- src/main/scala/input.scala
+++ src/main/scala/input.scala
@@ -7,3 +7,11 @@ trait Resource {
 object MockResource extends Resource {
   override def read: Iterator[String] = Iterator("apple", "applo")
 }
+
+class FileResource(filename: String) extends Resource {
+  import scala.io.Source
+  override def read:Iterator[String] = {
+    // TODO: close
+    Source.fromFile(filename).getLines
+  }
+}
