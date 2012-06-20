9ed43f3dd67ca25cfd3ed627550dd8f3eea123a5
takayuki murata <takayuki.for.public@gmail.com>
2012-06-20 21:51:00 +0900

    get extension to distinct file type

--- src/main/scala/input.scala
+++ src/main/scala/input.scala
@@ -4,19 +4,23 @@ import com.tk.libra.utils.Using
 
 trait Resource {
   def read: List[String]
+  def extension: String
 }
 
 object MockResource extends Resource {
   override def read: List[String] = List("apple", "applo")
+  override def extension: String = "java"
 }
 
-class FileResource(filename: String) extends Resource with Using with LineParser  {
+class FileResource(val filename: String) extends Resource with Using with LineParser  {
   import scala.io.Source
-  override def read:List[String] = {
+  override def read: List[String] = {
     using(Source.fromFile(filename)) { source =>
       source.getLines.toList.map { line =>
         parse(line)
       }.flatten
     }
   }
+
+  override def extension: String = filename.split('.').toList.last
 }
--- src/test/scala/FileResourceSpec.scala
+++ src/test/scala/FileResourceSpec.scala
@@ -5,7 +5,8 @@ import org.specs2.mutable._
 class FileResourceSpec extends Specification {
   "read" should {
     "simple file read" in {
-      new FileResource("./resource/test/simple.txt").read.toList == List("apple", "applo")
+      new FileResource("./resource/test/simple.txt").read.toList must_== List("apple", "applo")
+      new FileResource("./resource/test/simple.txt").extension must_== "txt"
     }
   }
 }
